b661714a3f99e95c4219ce22256cafdaf977f5c8
NoRage <max.chyncha@gmail.com>
2022-10-01 10:12:21 +0300

    Add JWT Authorization

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+
 from .serializers import PostSerializer
 from .models import Post
 from rest_framework import viewsets
@@ -7,4 +9,5 @@ from rest_framework import viewsets
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
 
